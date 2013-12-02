2b63d21ca9a4a77b0b0addf7090d6c15ab61fd5f
Yin Qu <me.yin.qu@gmail.com>
2013-12-02 10:08:26 -0600

    Moved scripts from master to a new branch scripts.

--- BigSemanticsJava/checkout.bat
+++
@@ -1,2 +0,0 @@
-git clone https://github.com/ecologylab/BigSemanticsWrapperRepository.git BigSemanticsWrapperRepository
-git clone https://github.com/ecologylab/BigSemanticsJava.git BigSemanticsJava
--- BigSemanticsJava/checkout_ssh.sh
+++
@@ -1,2 +0,0 @@
-git clone git@github.com:ecologylab/BigSemanticsWrapperRepository.git BigSemanticsWrapperRepository
-git clone git@github.com:ecologylab/BigSemanticsJava.git BigSemanticsJava
