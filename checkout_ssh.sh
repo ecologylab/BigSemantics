925c5922f0528bb4e966ab80a7c9b192aa3d0d51
Yin Qu <me.yin.qu@gmail.com>
2014-01-02 18:35:27 -0600

    Added scripts to master branch, to help set up environment. They were previously in another branch.

---
+++ checkout_ssh.sh
@@ -0,0 +1,4 @@
+git clone git@github.com:ecologylab/BigSemanticsWrapperRepository.git ../BigSemanticsWrapperRepository
+git clone git@github.com:ecologylab/BigSemanticsJava.git ../BigSemanticsJava
+git clone git@github.com:ecologylab/BigSemanticsService.git ../BigSemanticsService
+git clone git@github.com:ecologylab/BigSemanticsJavaScript.git ../BigSemanticsJavaScript
