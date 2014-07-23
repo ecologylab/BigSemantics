725b74b347b445cebd7b47fec42b2198d37af1eb
Yin Qu <me.yin.qu@gmail.com>
2014-07-22 22:45:56 -0500

    Removed scripts which are not useful any more. Updated submodules.

--- checkout.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/ecologylab/BigSemanticsWrapperRepository.git ../BigSemanticsWrapperRepository
-git clone https://github.com/ecologylab/BigSemanticsJava.git ../BigSemanticsJava
-git clone https://github.com/ecologylab/BigSemanticsService.git ../BigSemanticsService
-git clone https://github.com/ecologylab/BigSemanticsJavaScript.git ../BigSemanticsJavaScript
--- checkout_ssh.bat
+++
@@ -1,4 +0,0 @@
-git clone git@github.com:ecologylab/BigSemanticsWrapperRepository.git ../BigSemanticsWrapperRepository
-git clone git@github.com:ecologylab/BigSemanticsJava.git ../BigSemanticsJava
-git clone git@github.com:ecologylab/BigSemanticsService.git ../BigSemanticsService
-git clone git@github.com:ecologylab/BigSemanticsJavaScript.git ../BigSemanticsJavaScript
